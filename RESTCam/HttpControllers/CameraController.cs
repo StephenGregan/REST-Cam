497e32d80b6becc1836a8a1f0206370c68e63b69
StephenGregan <grego@live.ie>
2018-08-03 12:09:41 +0100

    initial commit

---
+++ RESTCam/HttpControllers/CameraController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+using RESTCam.Models;
+using RESTCam.Video;
+
+namespace RESTCam.HttpControllers
+{
+    public class CameraController : ApiController
+    {
+    }
+}
